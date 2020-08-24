e40cb1e1d6d870886a0064c2c656e4d779e45775
Kush Rastogi <krustey@gmail.com>
2020-08-24 00:47:42 -0700

    Added QoL improvements, changed keyword loading strategy depending on zoom level

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 let map;
+let marker;
 var geoKeywordsList;
+
+var gracePeriodMs = 2000;
+var waitingForGracePeriod = false;
+var gracePeriodTimeoutFn = null;
+var gracePeriodTimer = null;
 
 $(document).ready(function() {
 	$('#recommendations-list').on('click', '.recommendation', function() {
@@ -9,10 +15,40 @@ $(document).ready(function() {
 
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 37.7159, lng: -121.9101 },
-    zoom: 8
-  });
+	var defaultLat = 37.7159;
+	var defaultLng = -121.9101;
+
+  	map = new google.maps.Map(document.getElementById("map"), {
+    	center: { lat: defaultLat, lng: defaultLng },
+    	zoom: 8
+  	});
+
+  	marker = new google.maps.Marker({
+		position: {lat: defaultLat, lng: defaultLng},
+		map: map,
+		title: 'Loc'
+	});
+
+	// Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        map.setCenter(pos);
+        map.setZoom(12);
+        switchToNewLocation(pos.lat, pos.lng);
+
+      }, function() {
+      	handleLocationError(true);
+      	  switchToNewLocation(defaultLat, defaultLng);
+      });
+    } else {
+    	handleLocationError(false);
+    	switchToNewLocation(defaultLat, defaultLng);
+      // Browser doesn't support Geolocation
+    }
 
   map.addListener('center_changed', function() {
     // 3 seconds after the center of the map has changed, pan back to the
@@ -20,18 +56,63 @@ function initMap() {
     var lat = map.center.lat();
     var lng = map.center.lng();
 
+    scheduleDataReload(lat, lng);
+  });
+
+  map.addListener('zoom_changed', function() {
+    delayDataReload();
+  });
+
+  map.addListener('click', function(clickEvent) {
+    var lat = clickEvent.latLng.lat();
+    var lng = clickEvent.latLng.lng();
+
     switchToNewLocation(lat, lng);
   });
 }
 
+function handleLocationError(browserHasGeolocation) {
+	if (browserHasGeolocation) {
+		alert("Ran into error while trying to get your location.");
+	} else {
+		alert("Your browser doesn't support geolocation.");
+	}
+}
+
+function scheduleDataReload(lat, lng) {
+	if (waitingForGracePeriod) {
+		clearTimeout(gracePeriodTimer);
+	} else {
+		setLoadingPage();
+	}
+	waitingForGracePeriod = true;
+	gracePeriodTimeoutFn = function(){switchToNewLocation(lat, lng)};
+	delayDataReload();
+}
+
+function delayDataReload() {
+	if (waitingForGracePeriod) {
+		clearTimeout(gracePeriodTimer);
+		gracePeriodTimer = setTimeout(gracePeriodTimeoutFn, gracePeriodMs);
+	}
+}
+
+function setLoadingPage() {
+	showLoadingBar();
+	setWikiUrl("");
+}
+
 function switchToNewLocation(lat, lng) {
+	if (waitingForGracePeriod){
+		clearTimeout(gracePeriodTimer);
+	}
+	waitingForGracePeriod = false;
+
+	marker.setPosition( new google.maps.LatLng( lat, lng ) );
 	var data = reverseGeocodeLatLng(lat, lng);
     
     if (data != null) {
 	    data.then(response => {
-	    	showLoadingBar();
-	    	setWikiUrl("");
-
 	    	var results = response.results;
 	    	var geoKeywordsByLocationType = parseGeocodingResults(results);
 	    	geoKeywordsList = getGeoKeywordsList(geoKeywordsByLocationType);
@@ -175,22 +256,32 @@ function parseGeocodingResults(results) {
 			}
 		}
 	}
+	console.log(locationsAndTypes)
     return locationsAndTypes;
 }
 
+/*	Zoom Levels
+	20 or smaller - neighborhood
+	12 or smaller - city
+	10 or smaller - county
+	5 or smaller - state
+	1 or smaller - country
+*/
 function isAcceptableLocationType(locationType) {
-	switch(locationType) {
-		case "colloquial_area":
-		case "administrative_area_level_1":
-		case "administrative_area_level_2":
-		case "country":
-		case "political":
-		case "locality":
-			return true;
-		default:
-			return false;
+	var acceptableLocations;
+	var zoomLevel = map.getZoom();
+
+	if (zoomLevel >= 11) {
+		acceptableLocations = ["neighborhood", "sublocality", "locality", "colloquial_area", 
+		"administrative_area_level_1", "administrative_area_level_2", "country"];
+	} else {
+		acceptableLocations = ["locality", 
+		"administrative_area_level_1", "administrative_area_level_2", "country"];
 	}
+
+	return acceptableLocations.includes(locationType);
 }
+
 
 function reverseGeocodeLatLng(lat, lng) {
     var geocodingLink = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=AIzaSyBm-lj_8Dm-mGESYObrzWGcVd1Siue4iYk`;
